f93815eac8d77c500d9ce45cfc00dca93b5c5192
Vasily Kuznetsov <kvas.it@gmail.com>
2016-11-07 21:17:10 +0100

    Improve argument parsing and help messages

--- cli_mock/crecord.py
+++ cli_mock/crecord.py
@@ -27,14 +27,18 @@ def forward_stream(stream, out, log, prefix):
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Log output of command line utilities'
+        description='Record the output of command line utilities.',
+        epilog='In order to supply flags starting with "-" or "--" to '
+               'the invoked command, prefix the command with "--".'
     )
     parser.add_argument('-l', '--log', default='log.txt', help='log file')
-    parser.add_argument('cmd', nargs='+', help='command and options')
+    parser.add_argument('cmd', help='command to run')
+    parser.add_argument('opts', nargs='*', help='options to the command')
     args = parser.parse_args()
+    cmd = [args.cmd] + args.opts
     with io.open(args.log, 'a', encoding='utf-8') as log:
-        log.write('$ {}\n'.format(' '.join(args.cmd)))
-        proc = subprocess.Popen(args.cmd,
+        log.write('$ {}\n'.format(' '.join(cmd)))
+        proc = subprocess.Popen(cmd,
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE,
                                 universal_newlines=True)
